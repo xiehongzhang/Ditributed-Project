fd8ef46e15c455bd445675249520de20d97a695b
xhz <1453734003@qq.com>
2019-11-05 11:17:56 +0800

    创建了一个测试类

---
+++ imooc_video_admin/imooc_video_admin_mng/src/main/java/com/imooc/controller/Test.java
@@ -0,0 +1,19 @@
+/**
+* Title: Test.java  
+
+* Description   
+
+* @author xhz  
+
+* @date 2019年11月5日  
+ 
+ */
+package com.imooc.controller;
+
+/**
+ * @author 
+ * @description 
+ */
+public class Test {
+
+}
